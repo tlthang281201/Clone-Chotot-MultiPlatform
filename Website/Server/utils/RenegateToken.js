db69b56f303d76e19dc0d61f4bfb9d83ca2a8e3d
tlthang281201 <thang281201@gmail.com>
2023-08-22 21:25:34 +0700

    second commit

--- Website/Server/utils/RenegateToken.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const genegateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET,{
-        expiresIn: "30d"
-    });
-};
-export default genegateToken;
